7ae43629522ea7dcc42f41507056226e15462d8b
Дөлгөөн Мөнгөнсарнай <120318974+Disshook@users.noreply.github.com>
2025-05-07 18:10:10 +0800

    dasdsadsa

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,51 @@ export const metadata: Metadata = {
   icons: {
     icon: "/logo.png",
   },
-  metadataBase: new URL("https://jinstod.mn"), 
+  keywords: [
+    // mongol
+    "Монгол аялал",
+    "Солонгос аялал",
+    "групп аялал",
+    "захиалгат аялал",
+    "жинст од трэвел",
+    "жинст од аялал",
+    "аялал жуулчлал",
+    "Хөвсгөлийн аялал",
+    "Говийн аялал",
+    "Төвийн бүсийн аялал",
+    "виз зөвлөгөө",
+    "аялалын захиалга",
+    "нутгийн аялал",
+    "Монгол Солонгос жуулчлал",
+    "Хөвсгөл нуур аялал",
+    "Монголын аяллын компани",
+  
+    // angli
+    "Mongolia travel",
+    "Korea travel tours",
+    "custom tours Mongolia",
+    "group travel Mongolia",
+    "Jinst Od Travel",
+    "Mongolian travel agency",
+    "Huvsgul lake tour",
+    "Gobi desert tour",
+    "Central Mongolia travel",
+    "visa support services",
+    "Mongolia Korea tours",
+    "travel to Huvsgul",
+    "Gobi desert adventure",
+    "tailor-made Mongolian tours",
+    "domestic Mongolia tours",
+    "Mongolian nature travel"
+  ],
+  
+  metadataBase: new URL("https://jinstod.com"), 
   alternates: {
-    canonical: "https://jinstod.mn",
+    canonical: "https://jinstod.com",
     languages: {
-      "mn": "https://jinstod.mn/mn",
-      "ko": "https://jinstod.mn/kr",
+      "mn": "https://jinstod.com/mn",
+      "kr": "https://jinstod.com/kr",
+      "en": "https://jinstod.com/en",
     },
   },
   openGraph: {
@@ -37,7 +76,7 @@ export const metadata: Metadata = {
     siteName: "Jinst Od Travel",
     images: [
       {
-        url: "https://jinstod.mn/og-image.jpg", 
+        url: "https://jinstod.mn/logo.png", 
         width: 1200,
         height: 630,
         alt: "Jinst Od аяллын зураг",
@@ -51,7 +90,7 @@ export const metadata: Metadata = {
     title: "Jinst Od Travel | Аяллын үйлчилгээ",
     description:
       "Монгол, Солонгос чиглэлийн аялал жуулчлалын онцгой үйлчилгээ.",
-    images: ["https://jinstod.mn/twitter-image.jpg"], 
+    images: ["https://jinstod.mn/logo.png"], 
   },
  
 };
